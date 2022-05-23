1fe987a0b6ad37a729861ded11c870d6b5abff28
Vadim <v.marenov@bk.ru>
2022-05-23 15:30:41 +0300

    First commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.Random;
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private int min;
+    private int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int nextIndex = min;
+
+            @Override
+            public boolean hasNext() {
+                return nextIndex <= max ? true : false;
+            }
+
+            @Override
+            public Integer next() {
+                return nextIndex++;
+            }
+        };
+    }
+
+
+}
